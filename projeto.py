9dba4bedd71699be3dd0ca783ed8f7da5a459300
Rhawan81 <rhawanjs@gmail.com>
2025-08-27 16:31:00 -0300

    implementa a lógica de interação do usuário para adicionar tarefas

--- projeto.py
+++ projeto.py
@@ -8,4 +8,10 @@ print("""1.Adicionar Tarefas
 3.Marcar como Concluida
 4.Remover Tarefas
 5.Sair""")
-
+lista_tarefas = []
+while True:
+    
+    escolha_user = int(input('Informe oque quer fazer:'))
+    if escolha_user == 1:
+        tarefa = input('O que deseja Adicionar ?')
+        lista_tarefas.append(tarefa)
