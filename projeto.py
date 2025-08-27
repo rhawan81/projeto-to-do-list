fce39531286764530f7810bd429d2efa136b7240
Rhawan81 <rhawanjs@gmail.com>
2025-08-27 17:39:44 -0300

    Atualização de código

--- projeto.py
+++ projeto.py
@@ -15,3 +15,27 @@ while True:
     if escolha_user == 1:
         tarefa = input('O que deseja Adicionar ?')
         lista_tarefas.append(tarefa)
+        
+    if escolha_user == 2:
+        print('Tarefas:')
+        for i, tarefa in enumerate(lista_tarefas, start=1):
+            print(f'{i}. {tarefa}')   
+            
+    if escolha_user == 3:
+        tarefa_concluida = int(input('Informe o numero da tarefa que deseja marcar como concluida:'))
+        if 0 < tarefa_concluida <= len(lista_tarefas):
+            lista_tarefas[tarefa_concluida - 1] += ' - Concluida'
+        else:
+            print('Numero invalido')
+            
+    if escolha_user == 4:
+        tarefa_remover = int(input('Informe o numero da tarefa que deseja remover:'))
+        if 0 < tarefa_remover <= len(lista_tarefas):
+            lista_tarefas.pop(tarefa_remover - 1)
+        else:
+            print('Numero invalido')
+            
+    if escolha_user == 5:
+        print('Saindo do programa...')
+        break   
+    
