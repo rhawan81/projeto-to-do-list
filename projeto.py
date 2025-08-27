5ad04e2ab46ed02fdd90547123f2be9eed6d70d7
Rhawan81 <rhawanjs@gmail.com>
2025-08-27 16:18:07 -0300

    adicionei as funções

--- projeto.py
+++ projeto.py
@@ -1,1 +1,11 @@
-print('Hello world!')
+### Projeto Grupo 4
+##  - Membros: - Michel , Jessica , Nilson , Stenio , Elaine
+
+print('-=====Projeto To do List-========')
+
+print("""1.Adicionar Tarefas
+2.Mostrar Tarefas
+3.Marcar como Concluida
+4.Remover Tarefas
+5.Sair""")
+
