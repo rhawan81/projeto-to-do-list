136641bbfc6761a9859e76818ae6f23bb84953be
Rhawan81 <rhawanjs@gmail.com>
2025-08-27 17:41:20 -0300

    Novas funçoes adicionadas

--- projeto.py
+++ projeto.py
@@ -3,39 +3,50 @@
 
 print('-=====Projeto To do List-========')
 
-print("""1.Adicionar Tarefas
+lista_tarefas = []
+
+
+while True: ## Enquanto for verdadeiro ele ira se repetir
+
+    print("""1.Adicionar Tarefas
 2.Mostrar Tarefas
 3.Marcar como Concluida
 4.Remover Tarefas
 5.Sair""")
-lista_tarefas = []
-while True:
-    
-    escolha_user = int(input('Informe oque quer fazer:'))
+   ## mostrar as opçoes ao usuario
+    escolha_user = int(input('O que deseja Fazer ?'))
     if escolha_user == 1:
-        tarefa = input('O que deseja Adicionar ?')
-        lista_tarefas.append(tarefa)
+        adicionar = input('O que deseja Adicionar ?') .capitalize()
+        lista_tarefas.append(adicionar)
+        print(f'Tarefa {adicionar} foi  Adicionada com sucesso')
+    elif escolha_user == 2:
+        for  listar, tarefa in enumerate(lista_tarefas, start=1):
+            print(f'{listar}. {tarefa}')
+    elif escolha_user == 3:
+        if not lista_tarefas:
+            print("Nenhuma tarefa para marcar.")
+        else:
+        # 1. Mostra a lista inteira primeiro
+            print("Qual tarefa você deseja concluir?")
+            for i, t in enumerate(lista_tarefas, start=1):
+                print(f"{i}. {t}")
+
+        # 2. Depois que a lista acaba, pergunta ao usuário
+            numero_tarefa_str = input("Digite o número: ")
+            numero_int = int(numero_tarefa_str)
+            indice = numero_int - 1
+            tarefa_selecionada = lista_tarefas[indice]
+            print(f'Sua Tarefa {tarefa_selecionada} foi concluida com Sucesso !')
+
+    elif escolha_user == 4:
+        for mostrar , task in enumerate(lista_tarefas,start=1):
+            print(f'{mostrar}. {task}')
+        remover  = int(input('Qual tarefa voce quer remover'))
+        indice_remover = remover - 1
+        lista_tarefas.pop(indice_remover)
+        print(f'A Tarefa {lista_tarefas[indice_remover]} foi removida com Sucesso !')
         
-    if escolha_user == 2:
-        print('Tarefas:')
-        for i, tarefa in enumerate(lista_tarefas, start=1):
-            print(f'{i}. {tarefa}')   
-            
-    if escolha_user == 3:
-        tarefa_concluida = int(input('Informe o numero da tarefa que deseja marcar como concluida:'))
-        if 0 < tarefa_concluida <= len(lista_tarefas):
-            lista_tarefas[tarefa_concluida - 1] += ' - Concluida'
-        else:
-            print('Numero invalido')
-            
-    if escolha_user == 4:
-        tarefa_remover = int(input('Informe o numero da tarefa que deseja remover:'))
-        if 0 < tarefa_remover <= len(lista_tarefas):
-            lista_tarefas.pop(tarefa_remover - 1)
-        else:
-            print('Numero invalido')
-            
-    if escolha_user == 5:
-        print('Saindo do programa...')
-        break   
-    
+    elif escolha_user == 5:
+        print('------Saindo Programa , até Logo ------')
+        break    
+        
